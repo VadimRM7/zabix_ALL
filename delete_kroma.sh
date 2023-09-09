9f5cad74b342876dbc41727dcc9269f7b68bbb48
VadimRM7 <142230283+VadimRM7@users.noreply.github.com>
2023-09-09 19:03:46 +0300

    Create delete_kroma.sh

---
+++ delete_kroma.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+docker-compose -f $HOME/kroma-up/docker-compose.yml --profile validator down -v
+rm -rf kroma-up/
+docker volume rm kroma-up_db
+sudo -u geth /home/geth/eth-docker/ethd terminate
