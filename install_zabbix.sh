f5271775356c0e463cea27844b8f324308b4aa63
VadimRM7 <142230283+VadimRM7@users.noreply.github.com>
2024-09-26 17:46:59 +0200

    Update install_zabbix.sh

--- install_zabbix.sh
+++ install_zabbix.sh
@@ -2,6 +2,7 @@
 
 hostname="$HOST_NAME"
 server_ip="65.108.196.236"
+LOGFILE="/var/log/zabbix_install.log"
 
 if [[ -z "$hostname" ]]; then
     echo "Переменная окружения HOST_NAME не установлена"
@@ -9,19 +10,19 @@ if [[ -z "$hostname" ]]; then
 fi
 
 run_command() {
-    echo "Выполнение команды: sudo $@"
+    echo "Выполнение команды: sudo $@" | tee -a $LOGFILE
     sudo "$@"
     local status=$?
     if [ $status -ne 0 ]; then
-        echo "Ошибка при выполнении команды: $1"
-        exit $status
+        echo "Ошибка при выполнении команды: $1 (код ошибки: $status)"
+    else
+  echo "Команда успешно выполнена: $1"
     fi
-    echo "Команда успешно выполнена: $1"
     return $status
 }
 
 remove_old_zabbix(){
-    echo "Начало удаления старого агента Zabbix"
+    echo "Начало удаления старого агента Zabbix" | tee -a $LOGFILE
     if [[ $(systemctl list-units --full -all | grep "zabbix-agent" | wc -l) -gt 0 ]]; then
         run_command systemctl stop zabbix-agent
         run_command apt remove -y zabbix-agent
@@ -29,11 +30,11 @@ remove_old_zabbix(){
     if [[ -d "/etc/zabbix/" ]]; then
         run_command rm -rf /etc/zabbix/
     fi
-    echo "Старый агент Zabbix удалён"
+    echo "Старый агент Zabbix удалён" | tee -a $LOGFILE
 }
 
 install_zabbix(){
-    echo "Начало установки агента Zabbix"
+    echo "Начало установки агента Zabbix" | tee -a $LOGFILE
     if [[ ! -f "/etc/zabbix/zabbix_agentd.conf" ]]; then
         run_command apt remove --purge zabbix-agent -y
         run_command wget https://repo.zabbix.com/zabbix/6.4/ubuntu/pool/main/z/zabbix-release/zabbix-release_6.4-1+ubuntu20.04_all.deb
@@ -44,21 +45,21 @@ install_zabbix(){
         run_command systemctl enable zabbix-agent
         run_command rm -f "zabbix-release_6.4-1+ubuntu20.04_all.deb"
     fi
-    echo "Агент Zabbix установлен"
+    echo "Агент Zabbix установлен" | tee -a $LOGFILE
 }
 
 config_zabbix(){
-    echo "Начало конфигурации агента Zabbix"
+    echo "Начало конфигурации агента Zabbix" | tee -a $LOGFILE
     run_command sed -i "s/Hostname=.*/Hostname=$hostname/g" /etc/zabbix/zabbix_agentd.conf
     run_command sed -i "s/Server=.*/Server=$server_ip/g" /etc/zabbix/zabbix_agentd.conf
     run_command sed -i "s/ServerActive=.*/ServerActive=$server_ip/g" /etc/zabbix/zabbix_agentd.conf
     run_command sed -i "s/# HostMetadata=/HostMetadata=autoreg.linux/" /etc/zabbix/zabbix_agentd.conf
     run_command systemctl restart zabbix-agent
-    echo "Агент Zabbix сконфигурирован"
+    echo "Агент Zabbix сконфигурирован" | tee -a $LOGFILE
 }
 
-echo "Запуск скрипта"
+echo "Запуск скрипта" | tee -a $LOGFILE
 remove_old_zabbix
 install_zabbix
 config_zabbix
-echo "Скрипт успешно выполнен"
+echo "Скрипт успешно выполнен" | tee -a $LOGFILE
